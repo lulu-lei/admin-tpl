64ccf430412d8b0ccefa7abe0305a86e978e6f64
RossoCarlo <gummibears@163.com>
2021-05-19 23:00:01 +0800

    add readfile util

--- packages/admin-server/middleware/koa_response_data.js
+++ packages/admin-server/middleware/koa_response_data.js
@@ -1,4 +1,19 @@
 // 处理业务逻辑的中间件
+const path = require("path");
+const { getJsonData } = require("../utils/file");
+
 module.exports = async (ctx, next) => {
+    const url = ctx.request.url;
+    let filePath = url.replace("/api", "");
+    filePath = path.join(__dirname, "../data" + filePath + ".json");
+    try {
+        const result = await getJsonData(filePath);
+        ctx.response.body = result;
+    } catch (error) {
+        ctx.response.body = {
+            errorMessage: "不存在"
+        };
+    }
     
+    await next();
 }
