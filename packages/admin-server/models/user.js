eae34bed16e7ecb8e2f2653ec3493efd51b9b368
CarloLei <gummibears@163.com>
2021-05-26 17:42:09 +0800

    add mysql driver

---
+++ packages/admin-server/models/user.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('user', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    user_name: {
+      type: DataTypes.CHAR(50),
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.CHAR(128),
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    tableName: 'user',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+      {
+        name: "user_id_uindex",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
